5857e2279758a539a54ce79e0bb260feb8dbdfd9
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-01-07 09:22:09 -0600

    refactor: 💡 Adding an example

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,3 +1,20 @@
+<template>
+  <div id="app">
+    <vue-suspense v-model="dataLoaded" :promise="loadData()" :key="key">
+      <ul>
+        <li v-for="number in dataLoaded" :key="number">{{ number }}</li>
+      </ul>
+      <template #error="{error}">
+        {{ error }}
+      </template>
+      <!-- <template #loading>
+        Loading numbers...
+      </template> -->
+    </vue-suspense>
+    <button @click="keyFlag=!keyFlag">Reload</button>
+  </div>
+</template>
+
 <script lang="ts">
 import Vue from 'vue';
 import VueSuspense from '@/vue-suspense.vue';
@@ -6,12 +23,26 @@ export default Vue.extend({
   name: 'ServeDev',
   components: {
     VueSuspense
+  },
+  data: () => ({
+    keyFlag: false,
+    dataLoaded: [],
+  }),
+  computed: {
+    key() {
+      return this.keyFlag ? 1 : -1;
+    }
+  },
+  methods: {
+    async loadData() {
+      const randomSeconds: number = Math.floor(Math.random() * 6) + 1;
+      console.log(randomSeconds);
+      await new Promise((resolve) => setTimeout(resolve, randomSeconds * 1000));
+      if (randomSeconds >= 5) {
+        return Promise.reject('Timeout error');
+      }
+      return Array.from(Array(10), (_, index) => index + 1);
+    }
   }
 });
 </script>
-
-<template>
-  <div id="app">
-    <vue-suspense />
-  </div>
-</template>
