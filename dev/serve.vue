96f3974dfaa4895a17c8ff6af2adb5f21c536f61
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-01-07 07:44:19 -0600

    feat: 🎸 Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import VueSuspense from '@/vue-suspense.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueSuspense
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-suspense />
+  </div>
+</template>
